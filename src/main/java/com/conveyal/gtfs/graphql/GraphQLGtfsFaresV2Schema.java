fbf52822497ff58f572ea96c2d1fc26b8a4a9c7e
Robin Beer <robin.beer@arcadis.com>
2025-01-28 10:37:40 +0000

    refactor(Replaced camel case with snake case for GrpahQL objs):

--- src/main/java/com/conveyal/gtfs/graphql/GraphQLGtfsFaresV2Schema.java
+++ src/main/java/com/conveyal/gtfs/graphql/GraphQLGtfsFaresV2Schema.java
@@ -23,9 +23,19 @@ import static graphql.schema.GraphQLObjectType.newObject;
 
 public class GraphQLGtfsFaresV2Schema {
 
+    private static final String AREA_TYPE_NAME = "area";
+    private static final String STOP_AREA_TYPE_NAME = "stop_area";
+    private static final String TIME_FRAME_TYPE_NAME = "time_frame";
+    private static final String NETWORK_TYPE_NAME = "network";
+    private static final String ROUTE_NETWORK_TYPE_NAME = "route_network";
+    private static final String FARE_MEDIA_TYPE_NAME = "fare_media";
+    private static final String FARE_PRODUCT_TYPE_NAME = "fare_product";
+    private static final String FARE_LEG_RULE_TYPE_NAME = "fare_leg_rule";
+    private static final String FARE_TRANSFER_RULE_TYPE_NAME = "fare_transfer_rule";
+
     private GraphQLGtfsFaresV2Schema() {}
 
-    public static final GraphQLObjectType stopAreaType = newObject().name("stopArea")
+    public static final GraphQLObjectType stopAreaType = newObject().name(STOP_AREA_TYPE_NAME)
         .description("A GTFS stop area object")
         .field(MapFetcher.field("id", GraphQLInt))
         .field(MapFetcher.field(StopArea.AREA_ID_NAME))
@@ -33,15 +43,15 @@ public class GraphQLGtfsFaresV2Schema {
         .field(createFieldDefinition("stops", GraphQLGtfsSchema.stopType, "stops", StopArea.STOP_ID_NAME))
         .build();
 
-    public static final GraphQLObjectType areaType = newObject().name("area")
+    public static final GraphQLObjectType areaType = newObject().name(AREA_TYPE_NAME)
         .description("A GTFS area object")
         .field(MapFetcher.field("id", GraphQLInt))
         .field(MapFetcher.field(Area.AREA_ID_NAME))
         .field(MapFetcher.field(Area.AREA_NAME_NAME))
-        .field(createFieldDefinition("stopAreas", stopAreaType, StopArea.TABLE_NAME, Area.AREA_ID_NAME))
+        .field(createFieldDefinition("stop_areas", stopAreaType, StopArea.TABLE_NAME, Area.AREA_ID_NAME))
         .build();
 
-    public static final GraphQLObjectType timeFrameType = newObject().name("timeFrame")
+    public static final GraphQLObjectType timeFrameType = newObject().name(TIME_FRAME_TYPE_NAME)
         .description("A GTFS time frame object")
         .field(MapFetcher.field("id", GraphQLInt))
         .field(MapFetcher.field(TimeFrame.TIME_FRAME_GROUP_ID_NAME))
@@ -50,14 +60,14 @@ public class GraphQLGtfsFaresV2Schema {
         .field(MapFetcher.field(TimeFrame.SERVICE_ID_NAME))
         .build();
 
-    public static final GraphQLObjectType networkType = newObject().name("network")
+    public static final GraphQLObjectType networkType = newObject().name(NETWORK_TYPE_NAME)
         .description("A GTFS network object")
         .field(MapFetcher.field("id", GraphQLInt))
         .field(MapFetcher.field(Network.NETWORK_ID_NAME))
         .field(MapFetcher.field(Network.NETWORK_NAME_NAME))
         .build();
 
-    public static final GraphQLObjectType routeNetworkType = newObject().name("routeNetwork")
+    public static final GraphQLObjectType routeNetworkType = newObject().name(ROUTE_NETWORK_TYPE_NAME)
         .description("A GTFS route network object")
         .field(MapFetcher.field("id", GraphQLInt))
         .field(MapFetcher.field(RouteNetwork.NETWORK_ID_NAME))
@@ -65,7 +75,7 @@ public class GraphQLGtfsFaresV2Schema {
         .field(createFieldDefinition("networks", networkType, Network.TABLE_NAME, RouteNetwork.NETWORK_ID_NAME))
         .build();
 
-    public static final GraphQLObjectType fareMediaType = newObject().name("fareMedia")
+    public static final GraphQLObjectType fareMediaType = newObject().name(FARE_MEDIA_TYPE_NAME)
         .description("A GTFS fare media object")
         .field(MapFetcher.field("id", GraphQLInt))
         .field(MapFetcher.field(FareMedia.FARE_MEDIA_ID_NAME))
@@ -73,7 +83,7 @@ public class GraphQLGtfsFaresV2Schema {
         .field(MapFetcher.field(FareMedia.FARE_MEDIA_TYPE_NAME))
         .build();
 
-    public static final GraphQLObjectType fareProductType = newObject().name("fareProduct")
+    public static final GraphQLObjectType fareProductType = newObject().name(FARE_PRODUCT_TYPE_NAME)
         .description("A GTFS fare product object")
         .field(MapFetcher.field("id", GraphQLInt))
         .field(MapFetcher.field(FareProduct.FARE_PRODUCT_ID_NAME))
@@ -81,10 +91,10 @@ public class GraphQLGtfsFaresV2Schema {
         .field(MapFetcher.field(FareProduct.FARE_MEDIA_ID_NAME))
         .field(MapFetcher.field(FareProduct.AMOUNT_NAME))
         .field(MapFetcher.field(FareProduct.CURRENCY_NAME))
-        .field(createFieldDefinition("fareMedia", fareMediaType, FareMedia.TABLE_NAME, FareProduct.FARE_MEDIA_ID_NAME))
+        .field(createFieldDefinition(FARE_MEDIA_TYPE_NAME, fareMediaType, FareMedia.TABLE_NAME, FareProduct.FARE_MEDIA_ID_NAME))
         .build();
 
-    public static final GraphQLObjectType fareLegRuleType = newObject().name("fareLegRule")
+    public static final GraphQLObjectType fareLegRuleType = newObject().name(FARE_LEG_RULE_TYPE_NAME)
         .description("A GTFS fare leg rule object")
         .field(MapFetcher.field("id", GraphQLInt))
         .field(MapFetcher.field(FareLegRule.LEG_GROUP_ID_NAME))
@@ -98,27 +108,27 @@ public class GraphQLGtfsFaresV2Schema {
         // Will return either routes or networks, not both.
         .field(createFieldDefinition("routes", GraphQLGtfsSchema.routeType, Route.TABLE_NAME, FareLegRule.NETWORK_ID_NAME))
         .field(createFieldDefinition("networks", networkType, Network.TABLE_NAME, Network.NETWORK_ID_NAME))
-        .field(createFieldDefinition("fareProducts", fareProductType, FareProduct.TABLE_NAME, FareLegRule.FARE_PRODUCT_ID_NAME))
+        .field(createFieldDefinition("fare_products", fareProductType, FareProduct.TABLE_NAME, FareLegRule.FARE_PRODUCT_ID_NAME))
         // fromTimeFrame and toTimeFrame may return multiple time frames.
         .field(createFieldDefinition(
-            "fromTimeFrame",
+            "from_time_frame",
             timeFrameType,
             TimeFrame.TABLE_NAME,
             FareLegRule.FROM_TIMEFRAME_GROUP_ID_NAME,
             TimeFrame.TIME_FRAME_GROUP_ID_NAME
         ))
         .field(createFieldDefinition(
-            "toTimeFrame",
+            "to_time_frame",
             timeFrameType,
             TimeFrame.TABLE_NAME,
             FareLegRule.TO_TIMEFRAME_GROUP_ID_NAME,
             TimeFrame.TIME_FRAME_GROUP_ID_NAME
         ))
-        .field(createFieldDefinition("toArea", areaType, Area.TABLE_NAME, FareLegRule.TO_AREA_ID_NAME, Area.AREA_ID_NAME))
-        .field(createFieldDefinition("fromArea", areaType, Area.TABLE_NAME, FareLegRule.FROM_AREA_ID_NAME, Area.AREA_ID_NAME))
+        .field(createFieldDefinition("to_area", areaType, Area.TABLE_NAME, FareLegRule.TO_AREA_ID_NAME, Area.AREA_ID_NAME))
+        .field(createFieldDefinition("from_area", areaType, Area.TABLE_NAME, FareLegRule.FROM_AREA_ID_NAME, Area.AREA_ID_NAME))
         .build();
 
-    public static final GraphQLObjectType fareTransferRuleType = newObject().name("fareTransferRule")
+    public static final GraphQLObjectType fareTransferRuleType = newObject().name(FARE_TRANSFER_RULE_TYPE_NAME)
         .description("A GTFS fare transfer rule object")
         .field(MapFetcher.field("id", GraphQLInt))
         .field(MapFetcher.field(FareTransferRule.FROM_LEG_GROUP_ID_NAME))
@@ -127,17 +137,17 @@ public class GraphQLGtfsFaresV2Schema {
         .field(MapFetcher.field(FareTransferRule.DURATION_LIMIT_NAME))
         .field(MapFetcher.field(FareTransferRule.DURATION_LIMIT_TYPE_NAME))
         .field(MapFetcher.field(FareTransferRule.FARE_PRODUCT_ID_NAME))
-        .field(createFieldDefinition("toArea", areaType, Area.TABLE_NAME, FareLegRule.TO_AREA_ID_NAME, Area.AREA_ID_NAME))
-        .field(createFieldDefinition("fareProducts", fareProductType, FareProduct.TABLE_NAME, FareProduct.FARE_PRODUCT_ID_NAME))
+        .field(createFieldDefinition("to_area", areaType, Area.TABLE_NAME, FareLegRule.TO_AREA_ID_NAME, Area.AREA_ID_NAME))
+        .field(createFieldDefinition("fare_products", fareProductType, FareProduct.TABLE_NAME, FareProduct.FARE_PRODUCT_ID_NAME))
         .field(createFieldDefinition(
-            "fromFareLegRule",
+            "from_fare_leg_rule",
             fareLegRuleType,
             FareLegRule.TABLE_NAME,
             FareTransferRule.FROM_LEG_GROUP_ID_NAME,
             FareLegRule.LEG_GROUP_ID_NAME
         ))
         .field(createFieldDefinition(
-            "toFareLegRule",
+            "to_fare_leg_rule",
             fareLegRuleType,
             FareLegRule.TABLE_NAME,
             FareTransferRule.TO_LEG_GROUP_ID_NAME,
@@ -147,15 +157,15 @@ public class GraphQLGtfsFaresV2Schema {
 
     public static List<GraphQLFieldDefinition> getFaresV2FieldDefinitions() {
         return Arrays.asList(
-            createFieldDefinition("area", areaType, Area.TABLE_NAME),
-            createFieldDefinition("stopArea", stopAreaType, StopArea.TABLE_NAME),
-            createFieldDefinition("fareTransferRule", fareTransferRuleType, FareTransferRule.TABLE_NAME),
-            createFieldDefinition("fareProduct", fareProductType, FareProduct.TABLE_NAME),
-            createFieldDefinition("fareMedia", fareMediaType, FareMedia.TABLE_NAME),
-            createFieldDefinition("fareLegRule", fareLegRuleType, FareLegRule.TABLE_NAME),
-            createFieldDefinition("timeFrame", timeFrameType, TimeFrame.TABLE_NAME),
-            createFieldDefinition("network", networkType, Network.TABLE_NAME),
-            createFieldDefinition("routeNetwork", routeNetworkType, RouteNetwork.TABLE_NAME)
+            createFieldDefinition(AREA_TYPE_NAME, areaType, Area.TABLE_NAME),
+            createFieldDefinition(FARE_LEG_RULE_TYPE_NAME, fareLegRuleType, FareLegRule.TABLE_NAME),
+            createFieldDefinition(FARE_MEDIA_TYPE_NAME, fareMediaType, FareMedia.TABLE_NAME),
+            createFieldDefinition(FARE_PRODUCT_TYPE_NAME, fareProductType, FareProduct.TABLE_NAME),
+            createFieldDefinition(FARE_TRANSFER_RULE_TYPE_NAME, fareTransferRuleType, FareTransferRule.TABLE_NAME),
+            createFieldDefinition(NETWORK_TYPE_NAME, networkType, Network.TABLE_NAME),
+            createFieldDefinition(ROUTE_NETWORK_TYPE_NAME, routeNetworkType, RouteNetwork.TABLE_NAME),
+            createFieldDefinition(STOP_AREA_TYPE_NAME, stopAreaType, StopArea.TABLE_NAME),
+            createFieldDefinition(TIME_FRAME_TYPE_NAME, timeFrameType, TimeFrame.TABLE_NAME)
         );
     }
 }
