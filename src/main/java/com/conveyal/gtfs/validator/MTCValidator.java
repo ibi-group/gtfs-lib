5e004388b2473c391412fdd4954068c35186e231
Robin Beer <robin.beer@arcadis.com>
2025-05-13 12:39:33 +0100

    feat(Validate stop prefixes): MTC update to check for stop code prefixes
    Prefixes are defined via RTD

--- src/main/java/com/conveyal/gtfs/validator/MTCValidator.java
+++ src/main/java/com/conveyal/gtfs/validator/MTCValidator.java
@@ -3,10 +3,14 @@ package com.conveyal.gtfs.validator;
 import com.conveyal.gtfs.error.SQLErrorStorage;
 import com.conveyal.gtfs.loader.Feed;
 import com.conveyal.gtfs.model.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.conveyal.gtfs.error.NewGTFSErrorType.FIELD_VALUE_TOO_LONG;
+import static com.conveyal.gtfs.error.NewGTFSErrorType.MISSING_STOP_CODE_PREFIX;
 
 /**
  * MTCValidator runs a set of custom validation checks for GTFS feeds managed by MTC in Data Tools.
@@ -18,9 +22,24 @@ import static com.conveyal.gtfs.error.NewGTFSErrorType.FIELD_VALUE_TOO_LONG;
  * https://github.com/ibi-group/datatools-ui/issues/544.
  */
 public class MTCValidator extends FeedValidator {
+    private String primaryStopCodePrefix;
+    private List<String> secondaryStopCodePrefixes = new ArrayList<>();
+    private boolean hasStopPrefixes = false;
 
     public MTCValidator(Feed feed, SQLErrorStorage errorStorage) {
         super(feed, errorStorage);
+    }
+
+    public MTCValidator(
+        Feed feed,
+        SQLErrorStorage errorStorage,
+        String primaryStopPrefix,
+        List<String> secondaryStopPrefixes
+    ) {
+        super(feed, errorStorage);
+        hasStopPrefixes = true;
+        this.primaryStopCodePrefix = primaryStopPrefix;
+        this.secondaryStopCodePrefixes = secondaryStopPrefixes;
     }
 
     @Override
@@ -38,6 +57,12 @@ public class MTCValidator extends FeedValidator {
         for (Trip trip : feed.trips) {
             validateFieldLength(trip, trip.trip_headsign, 120);
             validateFieldLength(trip, trip.trip_short_name, 50);
+        }
+
+        if (hasStopPrefixes) {
+            for (Stop stop : feed.stops) {
+                validateStopCodePrefix(stop, stop.stop_code);
+            }
         }
     }
 
@@ -59,5 +84,32 @@ public class MTCValidator extends FeedValidator {
 
     public boolean validateFieldLength(Entity entity, URL url, int maxLength) {
         return validateFieldLength(entity, url != null ? url.toString() : "", maxLength);
+    }
+
+    /**
+     * Checks that the stop code contains primary and secondary prefixes.
+     * @param entity The containing GTFS entity (for error reporting purposes).
+     * @param stopCode The stop code to check for prefix.
+     * @return true if the stop code contains expected prefix or validation is skipped; false otherwise.
+     */
+    public boolean validateStopCodePrefix(Entity entity, String stopCode) {
+        if (stopCode == null || (primaryStopCodePrefix == null && secondaryStopCodePrefixes == null)) {
+            return true;
+        }
+
+        boolean hasPrefix = false;
+        if (!StringUtils.isBlank(primaryStopCodePrefix)) {
+            hasPrefix = stopCode.startsWith(primaryStopCodePrefix);
+        }
+        if (!hasPrefix && secondaryStopCodePrefixes != null && !secondaryStopCodePrefixes.isEmpty()) {
+            hasPrefix = secondaryStopCodePrefixes
+                .stream()
+                .anyMatch(stopCode::startsWith);
+        }
+        if (!hasPrefix && errorStorage != null) {
+            registerError(entity, MISSING_STOP_CODE_PREFIX, stopCode);
+        }
+
+        return hasPrefix;
     }
 }
